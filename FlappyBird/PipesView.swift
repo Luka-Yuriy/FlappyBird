cb825323aad21643a73ec64f06db8afdd3a7c987
Yuri Luka <lukayurii.work@gmail.com>
2024-07-29 22:49:31 +0500

    Work with view bird and pipes

---
+++ FlappyBird/PipesView.swift
@@ -0,0 +1,18 @@
+//
+//  PipesView.swift
+//  FlappyBird
+//
+//  Created by Yurii Luka on 29.07.2024.
+//
+
+import SwiftUI
+
+struct PipesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PipesView()
+}
