2baa516667a99e2632930a7f1f7bc29020d6936e
Yuri Luka <lukayurii.work@gmail.com>
2024-07-29 22:09:27 +0500

    Initial Commit

---
+++ FlappyBird/FlappyBirdApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlappyBirdApp.swift
+//  FlappyBird
+//
+//  Created by Yurii Luka on 29.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct FlappyBirdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
